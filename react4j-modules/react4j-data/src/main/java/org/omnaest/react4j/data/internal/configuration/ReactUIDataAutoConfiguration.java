1ddfaff23a57c1c01dc56c0d3f9b8e22cc11000f
omnaest <omnaest@web.de>
2024-02-06 20:19:51 +0100

    corrected RepositoryProvider bean creation

--- react4j-modules/react4j-data/src/main/java/org/omnaest/react4j/data/internal/configuration/ReactUIDataAutoConfiguration.java
+++ react4j-modules/react4j-data/src/main/java/org/omnaest/react4j/data/internal/configuration/ReactUIDataAutoConfiguration.java
@@ -16,16 +16,11 @@
 package org.omnaest.react4j.data.internal.configuration;
 
 import org.omnaest.react4j.data.Repository;
-import org.omnaest.react4j.data.annotations.EnableReactUIRepository;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackageClasses = Repository.class)
-@ConditionalOnWebApplication
-@ConditionalOnBean(annotation = EnableReactUIRepository.class)
 public class ReactUIDataAutoConfiguration
 {
 }
